97f16022b9411e0492f5eab3f28766cfc5b27ac0
igorv404 <igorvilkov404@gmail.com>
2022-07-24 22:43:33 +0300

    initial commit

---
+++ src/main/java/io/igorv404/bankhotelbackendspring/BankhotelBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package io.igorv404.bankhotelbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankhotelBackendSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankhotelBackendSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/igorv404/bankhotelbackendspring/BankhotelBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.igorv404.bankhotelbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankhotelBackendSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
